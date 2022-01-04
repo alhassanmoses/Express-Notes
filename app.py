a8229dd943f8443f986cf275bbc466c2601c1d93
Moses Wuniche Alhassan <Misty@MACs-MacBook-Pro.local>
2022-01-04 13:35:26 +0000

    added db integration and note model

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///test.db"
+
+db = SQLAlchemy(app)
+ 
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(50))
+    content = db.Column(db.String(500), nullable = False)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return "<Note %r>" %self.id 
 
 @app.route("/")
 def default():
-    return "Welcome to Flask"
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
